85778643ad1dcdfb1506f74fb1856a5d1b8d9029
arianna <arianna@Ubuntu20.04.myguest.virtualbox.org>
2024-05-06 12:25:37 +0200

    modified tests

---
+++ test/concurrency_test.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <string.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define DEVICE_PATH "/dev/reference_monitor" // Percorso del dispositivo nel sistema
+#define NUM_THREADS 10
+
+const char *states[] = {"ON", "REC_ON", "REC_OFF", "OFF"};
+
+void *perform_operation(void *args) {
+    char *operation = (char *)args;
+    int fd;
+    ssize_t bytes_written;
+
+    // Apri il dispositivo
+    fd = open(DEVICE_PATH, O_WRONLY);
+    if (fd < 0) {
+        perror("Failed to open the device");
+        pthread_exit(NULL);
+    }
+	printf("Thread %ld: %s\n",pthread_self(), operation);
+    bytes_written = write(fd, operation, strlen(operation));
+    
+    if (bytes_written < 0) {
+        perror("Failed to write to the device");
+    }
+
+    // Chiudi il dispositivo
+    close(fd);
+
+    pthread_exit(NULL);
+}
+
+int main(int argc, char** argv) {
+    pthread_t threads[NUM_THREADS];
+    int i;
+
+    // Inizializza il generatore di numeri casuali con un seed diverso per ogni esecuzione
+    srand(time(NULL));
+ char password[20];
+        printf("insert password to run the test\n");
+        fgets(password, sizeof(password), stdin);
+         // Rimuovi il newline inserito da fgets
+    password[strcspn(password, "\n")] = 0;
+    for (i = 0; i < NUM_THREADS; i++) {
+        // Crea la stringa operation per ogni thread
+        char operation[100];
+       
+        sprintf(operation, "%s %s %s", "new_state", states[rand() % 4], password);
+        
+        pthread_create(&threads[i], NULL, perform_operation, (void *)operation);
+    }
+
+    for (i = 0; i < NUM_THREADS; i++) {
+        pthread_join(threads[i], NULL);
+    }
+
+    return 0;
+}
+
