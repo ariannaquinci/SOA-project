bee408fb454239366cf50db0f8b911fbc54501cc
arianna <arianna@Ubuntu20.04.myguest.virtualbox.org>
2024-05-07 09:48:05 +0200

    modified tests again

--- test/test-rmdir.c
+++ test/test-rmdir.c
@@ -14,11 +14,7 @@ int main() {
     printf("Enter path to add:\n");
     fgets(path, sizeof(path), stdin);
     path[strcspn(path, "\n")] = 0;    
-   
-    char mkdir_command[200];
     
-    sprintf(mkdir_command, "mkdir -p %s/test_directory", path);
-    system(mkdir_command);
 
     fd = open(DEVICE_PATH, O_WRONLY);
     if (fd < 0) {
@@ -32,6 +28,19 @@ int main() {
     password[strcspn(password, "\n")] = 0;
     
     char change_state_command[100];
+    sprintf(change_state_command, "new_state OFF %s", password);
+    bytes_written = write(fd, change_state_command, strlen(change_state_command));
+    if (bytes_written < 0) {
+        perror("Failed to write to the device");
+        close(fd);
+        return 1;
+    }
+    char mkdir_command[200];
+    
+    sprintf(mkdir_command, "mkdir -p %s/test_directory", path);
+    system(mkdir_command);
+    
+   
     sprintf(change_state_command, "new_state REC_ON %s", password);
     bytes_written = write(fd, change_state_command, strlen(change_state_command));
     if (bytes_written < 0) {
